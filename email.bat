a8c81c42c7dd0b0b89cf3691f0c1726159c97185
Mihai Alexandra Gabriela <33200035+Mihai-Alexandra-Gabriela@users.noreply.github.com>
2022-06-21 12:47:42 +0300

    Update email.bat

--- email.bat
+++ email.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
-:: -- Set senvitive values and file attachment path
-SET "GmailAccount="
-SET "GmailPassword="
-SET "Attachment="
+:: -- Set senvitive values and file attachment path -> Powershell [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("..."))
+SET "GmailAccount=" :: -- Gmail account
+SET "GmailPassword=" :: -- Password
+SET "Attachment=" :: -- Path of the attachment
 
 :: -- Set other email values
-SET "EmailTo="
+SET "EmailTo=" :: -- Gmail account
 SET "EmailSubject=RAPORT"
 SET "EmailBody=SAMPLE FILE WITH ATTACHMENT"
 
